86dfbe7f4492a3a50a1dd380fe2a1ef3f2d6e312
bilalhzaidi <bilalhzaidi@protonmail.com>
2025-04-16 07:47:47 +0000

    Initial commit of Lawza project

---
+++ pages/login.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Login = () => {
+  return (
+    <div>
+      {/* Your login form or content */}
+      Login Page
+    </div>
+  );
+};
+
+export default Login;
---
+++ pages/verify-otp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const VerifyOtp = () => {
+  return (
+    <div>
+      {/* OTP verification form or content */}
+      Verify OTP Page
+    </div>
+  );
+};
+
+export default VerifyOtp;
